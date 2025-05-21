24df1354244fdd2be211a8011bcf6395f493912b
EC-Saloni <168294294+EC-Saloni@users.noreply.github.com>
2025-05-21 20:49:57 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,43 @@
+#Print strings 
+print("Hello World!!")
+print("She said,'Python is the easy language'")
+print('Hello, I am BCA 2nd year student.')
+
+#Taking user input of the string
+language = input("Enter your choosen programming language:")
+course = input("Enter your course:")
+
+#take int and float directly as input
+a = 4
+b = 11
+c = 8.0
+int_value = 99684
+float_value = 365.21497
+print(a)
+print(b)
+print(c)
+print(int_value)
+print(float_value)
+
+#Taking int and float as a user input
+d = int(input("Enter value of d:"))
+e = float(input("Enter value of e:"))
+
+#Convert string into int and float
+f = input("Enter value of f:")
+g = input("Enter value of g:")
+f1 = int(f)
+g1 = float(g)
+print(type(f1))
+print(type(g1))
+
+#Swap 2 variables
+num1 = 6
+num2 = 9
+num1,num2 = num2,num1
+print(f"value of num1: {num1}")
+print(f"value of num2: {num2}")
+
+#Round a float to 2 decimal places
+pi = 3.14159265359
+print(f"Print the value of pi in 2 decimal place:{pi:.2f} ")
